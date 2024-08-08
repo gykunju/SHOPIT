3b49572f2924dbeb6dbfa8ff88dd602b63a8f9b0
gykunju <alvingikunju@gmail.com>
2024-08-08 17:52:06 +0300

    Initial Commit

---
+++ src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    <div className="flex justify-between mx-10 bg-A1A1A0">
+      <a className="text-2xl font-bold">#SHOPIT</a>
+      <nav className="flex gap-4">
+        <button className="">LOG IN</button>
+        <button>Creat Account</button>
+      </nav>
+    </div>
+  );
+}
+
+export default Landing
